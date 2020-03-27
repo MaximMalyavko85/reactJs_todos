f0c5e0f83ecfdab845edeaecd6cedc04f48d11b9
maxim.malyavko <maxim.malyavko@foxtree.com>
2020-03-27 16:20:57 +0300

    add alert context

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-export const Notes = ({notes}) =>{
-
-
+export const Notes = ({ notes }) => {
   return (
-    <ul>
-      <li></li>
+    <ul class="list-group">
+      {notes.map(note => (
+        <li class="list-group-item note" key={note.id}>
+          <div>
+            <strong>{note.title}</strong>
+            <small>{new Date().toLocaleDateString()}</small>
+          </div>
+
+          <button type="button" class="btn btn-outline-danger btn-sm">
+            &times;
+          </button>
+        </li>
+      ))}
     </ul>
   );
-}
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Form } from "../components/Form";
+import { Notes } from "../components/Notes";
 
 export const Home = () => {
+  let notes = new Array(3)
+  .fill("")
+  .map((_, i)=>({id: i, title: `Note ${i}`}))
+  
+
   return (
     <React.Fragment>
       <Form />
+      <hr/>
+      <Notes notes = {notes}/>
     </React.Fragment>
   );
 };
